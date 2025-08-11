da6ea00bf35793978a331907c56b78a9538d1a0b
erikdejager <erik.dejager@outlook.com>
2025-08-11 14:07:17 +0200

    Calculators ready

--- src/components/CalculatorWithDraw.tsx
+++ src/components/CalculatorWithDraw.tsx
@@ -1,11 +1,160 @@
-import {Component} from "solid-js";
+import {Component, createMemo, createSignal} from "solid-js";
 
 
 const CalculatorWithDraw: Component = () => {
+
+    const [betSize, setBetSize] = createSignal<number>(100)
+
+    const [oneWinCount, setOneWinCount] = createSignal<number>(0)
+    const [drawCount, setdrawCount] = createSignal<number>(0)
+    const [twoWinCount, setTwoWinCount] = createSignal<number>(0)
+
+
+    const [oneMultiplier, setOneMultiplier] = createSignal<number>(0)
+    const [drawMultiplier, setDrawMultiplier] = createSignal<number>(0)
+    const [twoMultiplier, setTwoMultiplier] = createSignal<number>(0)
+
+    const oneOutcome = createMemo(() => betSize() * oneMultiplier());
+    const drawOutcome = createMemo(() => betSize() * drawMultiplier());
+    const twoOutcome = createMemo(() => betSize() * twoMultiplier());
+
+    const oneFrequency = createMemo(() => {
+        const total = oneWinCount() + twoWinCount() + drawCount();
+        return total > 0 ? oneWinCount() / total : 0;
+    });
+
+    const drawFrequency = createMemo(() => {
+        const total = oneWinCount() + twoWinCount() + drawCount();
+        return total > 0 ? drawCount() / total : 0;
+    });
+
+    const twoFrequency = createMemo(() => {
+        const total = oneWinCount() + twoWinCount() + drawCount();
+        return total > 0 ? twoWinCount() / total : 0;
+    });
+
+    const oneEV = createMemo(() => {
+        const valuationOne = oneOutcome() * oneFrequency()
+        const valuationTwo = (betSize() - betSize() * 2) * (twoFrequency() + drawFrequency())
+        return valuationOne + valuationTwo
+
+    })
+
+    const drawEV = createMemo(() => {
+        const valuationOne = drawOutcome() * drawFrequency()
+        const valuationTwo = (betSize() - betSize() * 2) * (oneFrequency() + twoFrequency())
+        return valuationOne + valuationTwo
+    }) // TODO
+
+    const twoEV = createMemo(() => {
+        const valuationOne = (betSize() - betSize() * 2) * (oneFrequency() + drawFrequency())
+        const valuationTwo = twoOutcome() * twoFrequency()
+        return valuationOne + valuationTwo
+    })
+
     return (
         <>
+            <p>Enter bet size</p>
+            <input type="number" placeholder="Bet size" class="input" value={betSize()} onInput={(e) => {
+                setBetSize(parseInt(e.target.value));
+            }} />
+
+            <div class={"grid grid-cols-3 gap-14 mt-10"}>
+                <div class={"flex flex-col gap-4"}>
+                    <h2 class={"text-xl"}>First Option</h2>
+
+                    <div>
+                        <p>Total wins</p>
+                        <input type="number" class="input w-full" value={oneWinCount()} onInput={(e) => {
+                            setOneWinCount(parseInt(e.target.value));
+                        }} />
+                    </div>
+
+                    <div>
+                        <p>Enter win multiplier (Format: 1,25)</p>
+                        <input type="number" step="any" placeholder="Format: 1,25" class="input w-full" value={oneMultiplier()} onInput={(e) => {
+                            setOneMultiplier(parseFloat(e.target.value));
+                        }} />
+                    </div>
+
+                    <div>
+                        <p>Potential profit</p>
+                        <input disabled type="number" step="any"  class="input w-full" value={oneOutcome()}/>
+                    </div>
+
+                    <div>
+                        <p>Odds of winning</p>
+                        <input disabled type="text" step="any" placeholder="Format: 1,25" class="input w-full" value={oneFrequency() * 100 + "%"}/>
+                    </div>
+                </div>
+
+                <div class={"flex flex-col gap-4"}>
+                    <h2 class={"text-xl"}>Draw Option</h2>
+
+                    <div>
+                        <p>Total draws</p>
+                        <input type="number" class="input w-full" value={drawCount()} onInput={(e) => {
+                            setdrawCount(parseInt(e.target.value));
+                        }} />
+                    </div>
+
+                    <div>
+                        <p>Enter draw multiplier (Format: 1,25)</p>
+                        <input type="number" step="any" placeholder="Format: 1,25" class="input w-full" value={drawMultiplier()} onInput={(e) => {
+                            setDrawMultiplier(parseFloat(e.target.value));
+                        }} />
+                    </div>
+
+                    <div>
+                        <p>Potential profit</p>
+                        <input disabled type="number" step="any"  class="input w-full" value={drawOutcome()}/>
+                    </div>
+
+                    <div>
+                        <p>Odds of winning</p>
+                        <input disabled type="text" step="any" placeholder="Format: 1,25" class="input w-full" value={drawFrequency() * 100 + "%"}/>
+                    </div>
+                </div>
+
+                <div class={"flex flex-col gap-4"}>
+                    <h2 class={"text-xl"}>Second Option</h2>
+
+                    <div>
+                        <p>Total wins</p>
+                        <input type="number" class="input w-full" value={twoWinCount()} onInput={(e) => {
+                            setTwoWinCount(parseInt(e.target.value));
+                        }} />
+                    </div>
+
+                    <div>
+                        <p>Enter win multiplier (Format: 1,25)</p>
+                        <input type="number" step="any" placeholder="Format: 1,25" class="input w-full" value={twoMultiplier()} onInput={(e) => {
+                            setTwoMultiplier(parseFloat(e.target.value));
+                        }} />
+                    </div>
+
+                    <div>
+                        <p>Potential profit</p>
+                        <input disabled type="number" step="any"  class="input w-full" value={twoOutcome()}/>
+                    </div>
+
+                    <div>
+                        <p>Odds of winning</p>
+                        <input disabled type="text" step="any" placeholder="Format: 1,25" class="input w-full" value={twoFrequency() * 100 + "%"}/>
+                    </div>
+                </div>
+            </div>
+
+            <div class="divider">EV</div>
+
+            <div class={"grid grid-cols-3 gap-14 mt-10"}>
+                <input disabled type="text" class="input w-full" value={oneEV()}/>
+                <input disabled type="text" class="input w-full" value={drawEV()}/>
+                <input disabled type="text" class="input w-full" value={twoEV()}/>
+            </div>
         </>
     )
 }
+
 
 export default CalculatorWithDraw;
